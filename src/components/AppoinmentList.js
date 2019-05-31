535128dc770b9db119a94fd1a41805bb05d0ef52
Leonardo Omaña <leotangram@gmail.com>
2019-05-30 20:23:36 -0500

    Add conditional message.

--- src/components/AppoinmentList.js
+++ src/components/AppoinmentList.js
@@ -2,22 +2,30 @@ import React from 'react'
 import Appoinment from './Appoinment'
 import PropTypes from 'prop-types'
 
-const AppoinmentList = ({ appoinments, deleteAppoinment }) => (
-  <div className='card mt-2 py-5'>
-    <div className='card-body'>
-      <h2 className='card-title text-center'>Administra las citas aquí</h2>
-      <div className='appoinment-list'>
-        {appoinments.map(appoinment => (
-          <Appoinment
-            key={appoinment.id}
-            appoinment={appoinment}
-            deleteAppoinment={deleteAppoinment}
-          />
-        ))}
+const AppoinmentList = ({ appoinments, deleteAppoinment }) => {
+  // Imprimir un mensaje en base a si hay citas o no
+  const message =
+    Object.keys(appoinments).length === 0
+      ? 'No hay citas'
+      : 'Administra las citas aquí'
+
+  return (
+    <div className='card mt-2 py-5'>
+      <div className='card-body'>
+        <h2 className='card-title text-center'>{message}</h2>
+        <div className='appoinment-list'>
+          {appoinments.map(appoinment => (
+            <Appoinment
+              key={appoinment.id}
+              appoinment={appoinment}
+              deleteAppoinment={deleteAppoinment}
+            />
+          ))}
+        </div>
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 AppoinmentList.propTypes = {
   appoinments: PropTypes.array.isRequired,
